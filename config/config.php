e6667cba3d57a46a329466cd49ea6974453da887
Ander <unladronv4@gmail.com>
2021-10-02 22:34:25 -0500

    inicio proyecto

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    define ('URL','http://dali.test/');
+    define ('HOST','localhost');
+    define ('DB','cooperativa');
+    define ('USER','trashy');
+    define ('PASSWORD','Ander123#@!');
+    define ('charset','utf8mb4');
---
+++ views/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+     T®ash °°°° 2021
+</div>
