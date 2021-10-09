196273dbc0eb21d5a4e861212b396a90d59e1784
LTrashy <73606918+LTrashy@users.noreply.github.com>
2021-10-08 22:02:58 -0500

    Delete config.php

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define ('URL','http://dali.test/');
-    define ('HOST','localhost');
-    define ('DB','cooperativa');
-    define ('USER','root');
-    define ('PASSWORD','donaire123#@!');
-    define ('charset','utf8mb4');
